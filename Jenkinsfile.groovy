b6c42fe2fffc52cbf1fb67a1e8562f845ad8d0b6
anasAZ22 <107927124+anasAZ22@users.noreply.github.com>
2023-05-19 17:11:55 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'npm install xxxx --legacy-peer-deps'
+                sh 'npm install --legacy-peer-deps'
             }
         }
         
